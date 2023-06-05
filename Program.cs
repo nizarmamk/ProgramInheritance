73b38b6cc9d933cb081062b96a9ffca81f72aed2
nizarmamk <nizarmbtp@students.amikom.ac.id>
2023-06-05 22:15:46 +0700

    Praktikum8

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Person1 = new Person();
+            Person1.Name = "Kowalski";
+            Person1.Age = 19;
+            Person1.InfoPerson();
+            Console.WriteLine();
+
+            // Inheritance
+            Karyawan Karyawan1 = new Karyawan();
+            Karyawan1.Name = "Julien";
+            Karyawan1.Age = 31;
+            Karyawan1.InfoPerson();
+            Karyawan1.InfoPerson("Dosen", "ABC12345");
+
+            Console.ReadLine();
+        }
+    }
+}
